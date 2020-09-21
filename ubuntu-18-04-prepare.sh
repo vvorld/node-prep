88f32a774c4202de11d7e7db92a9d200de26dd17
AnKekk <67998997+AnKekk@users.noreply.github.com>
2020-09-21 11:16:39 +0300

    update packages list

--- ubuntu-18-04-prepare.sh
+++ ubuntu-18-04-prepare.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+cd /tmp
 sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common ntp
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io nano mc prometheus-node-exporter
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose && sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo docker-compose --version
 
